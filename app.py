7120be4b6ddc1d3fdee9a021a216be0b298df92c
Potatoo-oo <jinwei030702@gmail.com>
2024-08-31 11:37:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def control_led():
     return jsonify({'status': f'LED is {action.upper()}'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
 
