ed548aa167afca72b3a1807c1e32592eb1956d57
Potatoo-oo <jinwei030702@gmail.com>
2024-08-31 12:26:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+# Initialize LED status
+led_status = 'off'  # Default LED status
+
 # Route to serve the HTML page
 @app.route('/')
 def index():
@@ -11,16 +14,24 @@ def index():
 # Endpoint to control the LED
 @app.route('/led', methods=['POST'])
 def control_led():
+    global led_status  # Use global variable to track LED status
     action = request.json.get('action')
-    # Logic to control the LED (this will interact with your ESP32)
+
     if action == 'on':
-        # Turn LED on
-        pass
+        led_status = 'on'  # Update LED status to 'on'
+        # Logic to turn LED on (if applicable to your setup)
+        # For example, you might send a signal to the ESP32 here
+
     elif action == 'off':
-        # Turn LED off
-        pass
-    return jsonify({'status': f'LED is {action.upper()}'})
+        led_status = 'off'  # Update LED status to 'off'
+        # Logic to turn LED off (if applicable to your setup)
+        # For example, you might send a signal to the ESP32 here
+
+    elif action == 'get_status':
+        # Return the current status of the LED
+        return jsonify({'status': led_status})
+
+    return jsonify({'status': f'LED is {led_status.upper()}'})
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
-
